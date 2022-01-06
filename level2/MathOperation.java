7889c29c53ac335472511bebc707a2ff8ff4181a
PJM <snf01003@naver.com>
2022-01-06 17:29:14 +0900

    ※ 프로그래머스(n개의 최소공배수) : 다른사람풀이

--- level2/MathOperation.java
+++ level2/MathOperation.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 public class MathOperation {
 
+    //N개의 최소공배수(나의 풀이)
     static int[] arr = {2,6,8,14};
     public static int solution(int[] arr) {
         int answer=1;
@@ -40,7 +41,23 @@ public class MathOperation {
         return answer;
     }
 
+    //N개의 최소공배수(다른 사람 풀이)
+    public static long nlcm(int[] num) {
+        long answer = num[0],g;
+        for(int i=1;i<num.length;i++){
+            g=gcd(answer,num[i]);
+            answer=g*(answer/g)*(num[i]/g);
+        }
+        return answer;
+    }
+    public static long gcd(long a,long b){
+        if(a>b)
+            return (a%b==0)? b:gcd(b,a%b);
+        else
+            return (b%a==0)? a:gcd(a,b%a);
+    }
+
     public static void main(String[] args) {
-        System.out.println(solution(arr));
+        System.out.println(nlcm(arr));
     }
 }
