e490a8ea6371a47a6a1ed1ff59ca347ddab8b748
PJM <snf01003@naver.com>
2022-01-06 10:58:51 +0900

    ※ 프로그래머스(디스크 컨트롤러) : Heap(우선순위 큐) 알고리즘

---
+++ level3/HeapPriorityQueue.java
@@ -0,0 +1,16 @@
+package level3;
+
+public class HeapPriorityQueue {
+
+    //디스크 컨트롤러
+    static int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
+    public static int solution(int[][] jobs) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(jobs));
+    }
+}
