5b46b4779eac4f6cf5f5d6fd849274a73ca17d12
PJM <snf01003@naver.com>
2022-01-06 14:26:42 +0900

    ※ 프로그래머스(디스크 컨트롤러) : 정답을 맞추는데 실패. 레벨2부터 우선 풀 필요가 있음.

---
+++ object/level3Object.java
@@ -0,0 +1,15 @@
+package object;
+
+public class level3Object {
+    public static class Jobs {
+        public int[] job;
+
+        public Jobs(int[] job) {
+            this.job = job;
+        }
+
+        public void moving() {
+
+        }
+    }
+}
