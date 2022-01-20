cceb0377045132de4a09a8d4a0557f1f493c1be1
snf01003 <snf01003@naver.com>
2022-01-20 17:28:01 +0900

    프로그래머스 1단계 풀었던 것 커밋

---
+++ level2/orther/StackAndQueue2.java
@@ -0,0 +1,28 @@
+package level2.orther;
+
+import object.level2Object.Truck;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+public class StackAndQueue2 {
+
+    //더 맵게(heap)
+//    static int[] scoville = {1, 2, 3, 9, 10, 12};
+//    static int K = 7;
+    static int[] scoville = {1,1};
+    static int K = 4;
+    public static int solution(int[] scoville, int K) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public void main(String[] args) {
+        System.out.println(solution(scoville, K));
+    }
+
+}
