ea7b14bf9fd9e84f403360734cf3d5da6c944472
snf01003 <snf01003@naver.com>
2022-03-07 17:59:48 +0900

    코딩중

--- level1/me/GetReportResults.java
+++ level1/me/GetReportResults.java
@@ -1,31 +1,43 @@
 package level1.me;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class GetReportResults {
     static String[] id_list = {"muzi", "frodo", "apeach", "neo"};
     static String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
     static int k = 2;
-//    String[] id_list = {"con", "ryan"};
-//    String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
-//    int k = 3;
+//    static String[] id_list = {"con", "ryan"};
+//    static String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
+//    static int k = 3;
 
     // 신고 결과 받기
     public static int[] solution(String[] id_list, String[] report, int k) {
         int idListLen = id_list.length;
         int[] answer = new int[idListLen];
-        HashMap<String, String> reportChkCnt = new HashMap();
-        HashMap<String, HashMap<String, String>> hashMap = new HashMap<>();
+        HashMap<String, ArrayList<String>> reportChk = new HashMap<>();
 
         for(String str : report) {
+            ArrayList<String> list = new ArrayList<>();
             String[] strArr = str.split(" ");
 
-            // 신고당하면 카운트 +1
-            reportChkCnt.put(strArr[0], strArr[1]);
+            if(reportChk.get(strArr[0]) != null) {
+                for(int i=0; i<list.size(); i++) {
+                    list.add(reportChk.get(strArr[0]).get(i));
+                }
+                list.add(strArr[1]);
+            } else {
+                list.add(strArr[1]);
+            }
 
-            // 같은 사람한테 신고를 당한 기록이 있으면 카운트 -1
+            reportChk.put(strArr[0], list);
         }
 
+        System.out.println(reportChk);
+
+        // 신고당하면 카운트 +1
+        // 같은 사람한테 신고를 당한 기록이 있으면 카운트 -1
 
         // k번 신고를 당하면 정지
 
@@ -36,5 +48,15 @@ public class GetReportResults {
 
     public static void main(String[] args) {
         solution(id_list, report, k);
+//        ArrayList<String> list = new ArrayList<>();
+//        HashMap<String, ArrayList<String>> hashMap = new HashMap<>();
+//
+//        list.add("frodo");
+//        list.add("neo");
+//
+//        hashMap.put("muzi",list);
+//
+//        System.out.println(list);
+//        System.out.println(hashMap.get("muzi").get(0));
     }
 }
