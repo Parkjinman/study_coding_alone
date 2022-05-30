b589a47adf6e28a60af33f1044a28bf8c12afa56
snf01003 <snf01003@naver.com>
2022-05-30 17:01:33 +0900

    JadenCase 문자열 만들기

---
+++ level2/me/JadenCase.java
@@ -0,0 +1,55 @@
+package level2.me;
+
+public class JadenCase {
+
+    static String solution(String s) {
+        String answer = "";
+
+        String[] arr = s.split("\\s", -1);
+
+        int cnt = 0;
+
+        for(String str : arr) {
+            cnt++;
+
+            if(!str.equals("")) {
+                answer += str.substring(0, 1).toUpperCase();
+                answer += str.substring(1).toLowerCase();
+
+                if(cnt != arr.length) answer += " "; else break;
+            } else {
+                if(cnt == arr.length) break; else answer += " ";
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("people  unFollowed   me"));
+        System.out.println(solution("3peoPle 2unFollowed  2me"));
+        System.out.println(solution("for the last week"));
+        System.out.println(solution(" for  the   last    week     "));
+
+//        String answer = "";
+//        String s = " for  the   last    week     ";
+//        s = s.trim();
+//
+//        String[] arr = s.split("\\s");
+//
+//        int cnt = 0;
+//
+//        for(String str : arr) {
+//            cnt++;
+//            str = str.trim();
+//            if(!str.equals("")) {
+//                answer += str.substring(0, 1).toUpperCase();
+//                answer += str.substring(1).toLowerCase();
+//
+//                if(cnt != arr.length) answer += " "; else break;
+//            } else {
+//                answer += " ";
+//            }
+//        }
+    }
+}
