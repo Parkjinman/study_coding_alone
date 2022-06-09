7b2343eab54e980af1abfcc96db21723e2df5ed0
snf01003 <snf01003@naver.com>
2022-06-09 17:26:29 +0900

    124나라의 숫자

---
+++ level2/me/NumberCountries124.java
@@ -0,0 +1,51 @@
+package level2.me;
+
+public class NumberCountries124 {
+    static String solution(int n) {
+        String answer = "";
+
+        System.out.print(Integer.toBinaryString(n));
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(1));  // 1
+        System.out.println(solution(2));  // 2
+        System.out.println(solution(3));  // 4
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.println(solution(4));  // 11
+        System.out.println(solution(5));  // 12
+        System.out.println(solution(6));  // 14
+        System.out.println(solution(7));  // 21
+        System.out.println(solution(8));  // 22
+        System.out.println(solution(9));  // 24
+        System.out.println(solution(10));  // 41
+        System.out.println(solution(11));  // 42
+        System.out.println(solution(12));  // 44
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.println(solution(13));  // 111
+        System.out.println(solution(14));  // 112
+        System.out.println(solution(15));  // 114
+        System.out.println(solution(16));  // 121
+        System.out.println(solution(17));  // 122
+        System.out.println(solution(18));  // 124
+        System.out.println(solution(19));  // 141
+        System.out.println(solution(20));  // 142
+        System.out.println(solution(21));  // 144
+        System.out.println(solution(22));  // 211
+        System.out.println(solution(23));  // 212
+        System.out.println(solution(24));  // 214
+        System.out.println(solution(25));  // 214
+        System.out.println(solution(26));  // 214
+        System.out.println(solution(27));  // 214
+        System.out.println(solution(28));  // 214
+        System.out.println(solution(29));  // 214
+        System.out.println(solution(30));  // 214
+        System.out.println(solution(31));  // 214
+        System.out.println(solution(32));  // 214
+        System.out.println(solution(33));  // 214
+        System.out.println(solution(34));  // 214
+        System.out.println(solution(35));  // 214
+    }
+}
