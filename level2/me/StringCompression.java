c4715a4c3898e8dbb9bb97ff7d7b1402e67751c4
snf01003 <snf01003@naver.com>
2022-06-14 17:24:45 +0900

    문자열압축 (일단 보류)

--- level2/me/StringCompression.java
+++ level2/me/StringCompression.java
@@ -2,19 +2,24 @@ package level2.me;
 
 public class StringCompression {
     static int solution(String s) {
-        String str = "";
+        int answer = 0;
+        int unitLen = s.length()/2;
+        String temp = "";
 
-        for(int i=2; i<=s.length(); i++) {
-            if(s.split(s.substring(0,s.length()/i)).length == 0) {
-                str = i+s.substring(0,s.length()/i);
+        for(int i=0; i<unitLen  ; i++) {
+            s.replace(s.substring(0, i), "");
+
+            if(s.contains(s.substring(0, i))) {
+                temp += s;
             }
         }
 
-        return str.length();
+        return answer;
     }
 
     public static void main(String[] args) {
-        String s = "aabbaccc"; //7
+//        String s = "aabbaccc"; //7
+        String s = "aabbaabb"; //7
 //        String s = "ababcdcdababcdcd"; //9
 //        String s = "abcabcdede"; //8
 //        String s = "abcabcabcabcdededededede"; //14
