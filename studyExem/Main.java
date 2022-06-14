f8b5c17fa10a26e5a30e7f9d9f05bc4b5366e1e9
snf01003 <snf01003@naver.com>
2022-06-14 17:49:08 +0900

    ..

---
+++ studyExem/Main.java
@@ -0,0 +1,25 @@
+package studyExem;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+
+        System.out.println(str+"??!");
+    }
+
+    //14 30
+    //20
+    //result : 14 50
+
+    //17 40
+    //80
+    //result : 19 0
+
+    //23 48
+    //25
+    //result : 0 13
+}
