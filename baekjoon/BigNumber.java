b5674c5b9cd38b6bd3c01232eaef8e468f99a726
snf01003 <snf01003@naver.com>
2022-06-20 14:12:06 +0900

    큰 수 A+B

---
+++ baekjoon/BigNumber.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String inputData = scan.nextLine();
+
+        String[] arr = inputData.split(" ");
+        BigInteger A = new BigInteger(arr[0]);
+        BigInteger B = new BigInteger(arr[1]);
+
+        A = A.add(B);
+
+        System.out.println(A.toString());
+    }
+}
