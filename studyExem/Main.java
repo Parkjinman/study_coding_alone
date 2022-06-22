cc661b21c651c70fe8da7abbe3a72ddb73b38582
snf01003 <snf01003@naver.com>
2022-06-22 17:59:21 +0900

    11866번] 요세푸스 문제

--- studyExem/Main.java
+++ studyExem/Main.java
@@ -1,34 +1,37 @@
 package studyExem;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-//    static ArrayList<int[]> list = new ArrayList<>();
+    static Queue<Integer> queue = new LinkedList<>();
+    static ArrayList<Integer> list = new ArrayList<>();
 //    static ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
-    static HashMap<String, Integer> xMap = new HashMap<>();
+//    static HashMap<Integer, Integer> xMap = new HashMap<>();
 //    static HashMap<String, Integer> yMap = new HashMap<>();
 //    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        int count = scan.nextInt();
-        String inputData = scan.nextLine();
-        String[] compare = new String[count];
+        int N = scan.nextInt();
+        int K = scan.nextInt();
+        int temp = K;
 
-        int k=0;
-        for(int i=0; i<count; i++) {
-            inputData = scan.nextLine();
-            String[] arr = inputData.split(" ");
-
-            compare[k] = arr[0];
-            xMap.put(arr[0], Integer.valueOf(arr[1]));
-
-            k++;
+        for(int i=1; i<=N; i++) {
+            list.add(i);
         }
-        System.out.println(Arrays.toString(compare));
-        System.out.println(xMap);
+
+        for(int i=0; i<N; i++) {
+            queue.add(list.get(K-1));
+            for(int j=0; j<temp; j++) {
+                if(K == queue.size()) K=1;
+                else K++;
+            }
+        }
+
+        System.out.println(queue);
     }
 }
 
