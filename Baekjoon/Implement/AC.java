a20242a0711c10d278cf30b3dc396fdd2a2b6f76
snf01003 <snf01003@naver.com>
2022-07-06 14:11:09 +0900

    5430] AC(덱 자료구조 발견)

--- Baekjoon/Implement/AC.java
+++ Baekjoon/Implement/AC.java
@@ -1,27 +1,79 @@
 package Baekjoon.Implement;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 
 public class AC {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static ArrayDeque<String> deque = new ArrayDeque<>();
+    public static void main(String[] args) throws IOException {
+//        Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.println("Stack!!");
-        ArrayDeque<String> stack = new ArrayDeque<>();
-        stack.addFirst("Element1");
-        stack.addFirst("Element2");
-        stack.addFirst("Element3");
-        System.out.println(stack.removeFirst());
-        System.out.println(stack.removeFirst());
-        System.out.println(stack.removeFirst());
+        int T = Integer.parseInt(br.readLine());
 
-        System.out.println("Queue!!");
-        ArrayDeque<String> queue = new ArrayDeque<>();
-        queue.addFirst("Element1");
-        queue.addFirst("Element2");
-        queue.addFirst("Element3");
-        System.out.println(queue.removeLast());
-        System.out.println(queue.removeLast());
-        System.out.println(queue.removeLast());
+        while (T-- > 0) {
+            try {
+                String P = br.readLine();
+                int N = Integer.parseInt(br.readLine());
+                String str = br.readLine();
+
+                String[] arrStr = str.split("[^0-9]");
+
+                for (String s : arrStr) {
+                    if (!s.equals(""))
+                        deque.addLast(s);
+                }
+                boolean dqSe = true;
+                sb.append("[");
+
+                String[] arrP = P.split("");
+                for (String s : arrP) {
+                    switch (s) {
+                        case "R":
+                            if(dqSe) dqSe = false;
+                            else dqSe = true;
+
+                            break;
+                        case "D":
+                            if(dqSe) deque.removeFirst();
+                            else deque.removeLast();
+                            break;
+                    }
+                }
+
+                if(!dqSe) {
+                    int dqSize = deque.size();
+                    String[] dqArr = new String[dqSize];
+                    for (int i = 0; i < dqSize; i++) {
+                        dqArr[i] = deque.removeLast();
+                    }
+
+                    for (int i = 0; i < dqSize; i++) {
+                        if(i == dqSize-1) sb.append(dqArr[i]);
+                        else sb.append(dqArr[i]+",");
+                    }
+                } else {
+                    while(!deque.isEmpty()) {
+                        if(deque.size() != 1) sb.append(deque.removeFirst()+",");
+                        else sb.append(deque.removeLast());
+                    }
+                }
+                sb.append("]\n");
+
+                System.out.print(sb);
+                deque.clear();
+                sb.setLength(0);
+            } catch (Exception e) {
+                sb.setLength(0);
+                sb.append("error\n");
+                System.out.print(sb);
+                sb.setLength(0);
+            }
+        }
+        br.close();
     }
 
     /*static ArrayList<Integer> list = new ArrayList<>();
