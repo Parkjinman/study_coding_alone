3efb113b2182a6b64b89cbec84c489021fe0416d
snf01003 <snf01003@naver.com>
2022-07-11 17:57:12 +0900

    2839] 설탕 배달

--- studyExem/Main.java
+++ studyExem/Main.java
@@ -1,8 +1,6 @@
 package studyExem;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
@@ -21,46 +19,28 @@ public class Main {
 //    static int[][] positionArr = null;
     static ArrayDeque<Integer> deque = new ArrayDeque<>();
 
+    static int solution(int num, int five, int three) {
+        if(num == 0) return five+three;
+        else if(num < 0) return -1;
+
+        if( (num%5 == 3 || num%5 == 0) && num%5 != 4 && num%5 != 1) {
+            num -= 5;
+            five++;
+        } else if(num%3 == 0) {
+            num -= 3;
+            three++;
+        }
+
+        return solution(num, five, three);
+    }
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
 
-        int N = scan.nextInt();
-        int M = scan.nextInt();
-        int position = 0;
-        int positionCp = 0;
-        int result = 0;
-        int n = N/2;
+        int bagCnt = scan.nextInt();
+        int five = 0;
+        int three = 0;
 
-        for(int i=0; i<N; i++) {
-            deque.addLast(i+1);
-        }
-
-        while(M --> 0) {
-            position = scan.nextInt();
-
-            Iterator<Integer> iterator = deque.iterator();
-            int i=1;
-            while(iterator.hasNext()) {
-                if(iterator.next() == position) positionCp = iterator.next();
-                i++;
-            }
-
-            while(position != deque.getFirst()) {
-                //왼쪽
-                if(positionCp < n) deque.addLast(deque.removeFirst());
-                //오른쪽
-                else deque.addFirst(deque.removeLast());
-
-                result++;
-            }
-
-            if(position == deque.getFirst()) {
-                deque.removeFirst();
-            }
-        }
-
-        System.out.println(deque.getClass());
-        System.out.println(result);
+        System.out.println(solution(bagCnt, five, three));
     }
 }
 
