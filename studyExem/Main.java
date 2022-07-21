47ff0115acab5611ef73eb935ad3d0bb3182495e
PJM <snf01003@naver.com>
2022-07-22 01:26:10 +0900

    ..

--- studyExem/Main.java
+++ studyExem/Main.java
@@ -3,6 +3,7 @@ package studyExem;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -21,40 +22,50 @@ public class Main {
 
 //    static int[][] positionArr = null;
 //    static ArrayDeque<Integer> deque = new ArrayDeque<>();
-    static int zero = 0;
-    static int one = 0;
 
-    static int fibonacci(int n) {
-        if(n==0) {
-            zero++;
-            return 0;
-        } else if(n==1) {
-            one++;
-            return 1;
-        } else {
-            return fibonacci(n-1) + fibonacci(n-2);
-        }
-    }
     public static void main(String[] args) throws IOException {
 //        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 //        int A = Integer.parseInt(st.nextToken());
 
+        HashMap map = new HashMap();
         Scanner scan = new Scanner(System.in);
         int T = scan.nextInt();
 
-        // 성능을 향상시키려면 DP의 메모제이션을 활용한 설계 개선 필요
-        while(T-->0) {
-            int N = scan.nextInt();
-            zero = 0;
-            one = 0;
+        int[] arr = new int[T];
+        int result = 0;
 
-            fibonacci(N);
-            System.out.println(zero+" "+one);
+        for(int i=0; i<T; i++) {
+            int red = scan.nextInt();
+            int green = scan.nextInt();
+            int blue = scan.nextInt();
+
+            map.put(red, "red");
+            map.put(green, "green");
+            map.put(blue, "blue");
+
+            if(i == 0) {
+                arr[i] = Math.min(red, green);
+                arr[i] = Math.min(arr[i], blue);
+            }
+
+            if(i-1 >= 0) {
+                if (map.get(arr[i - 1]).equals("red")) {
+                    arr[i] = Math.min(green, blue);
+                } else if (map.get(arr[i - 1]).equals("green")) {
+                    arr[i] = Math.min(red, blue);
+                } else if (map.get(arr[i - 1]).equals("blue")) {
+                    arr[i] = Math.min(red, green);
+                }
+            }
+
+            result += arr[i];
         }
 
+        System.out.println(result);
 
     }
 }
+
 
 //        while(true) {
 //            String[] arr = br.readLine().split(" ");
