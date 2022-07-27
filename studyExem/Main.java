77ac236e92f46be80c68e10f425e0bfa3f2d1b86
snf01003 <snf01003@naver.com>
2022-07-27 13:05:26 +0900

    ..

--- studyExem/Main.java
+++ studyExem/Main.java
@@ -7,28 +7,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
-        int K = scan.nextInt();
+        int M = scan.nextInt();
+        int V = scan.nextInt();
 
         /** 시간측정 start */
 //        long beforeTime = System.currentTimeMillis();
 
-        int[] arr = new int[N];
-        for(int i=0; i<N; i++) {
-            arr[i] = scan.nextInt();
+        while( M --> 0 ) {
+            int first = scan.nextInt();
+            int second = scan.nextInt();
         }
-
-        int result = 0;
-        for(int i=N-1; i>0; i--) {
-            if(K == 0) break;
-
-            if(K - arr[i] >= 0) {
-                K -= arr[i];
-                result ++;
-                i = N;
-            }
-        }
-
-        System.out.println(result);
 
         /** 시간측정 end */
 //        long afterTime = System.currentTimeMillis();
