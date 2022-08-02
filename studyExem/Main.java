15761edfa8b88ff4f3ac5bde769909fcdb5ece8b
snf01003 <snf01003@naver.com>
2022-08-02 13:08:04 +0900

    ..

--- studyExem/Main.java
+++ studyExem/Main.java
@@ -7,42 +7,65 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
+        String[][] strArr = new String[N][N];
 
+        int removePostion = N/3;
 
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                if(removePostion == i && removePostion == j) strArr[i][j] = " ";
+                else strArr[i][j] = "*";
+                System.out.print(strArr[i][j]);
+            }
+            System.out.println();
+        }
 
     }
 }
-/** 시간측정 start */
-//        long beforeTime = System.currentTimeMillis();
 
+/** 3 */
+//        ***
+//        * *
+//        ***
+// [1][1]
 
+/** 9 */
+//        *********
+//        * ** ** *
+//        *********
+//        ***   ***
+//        * *   * *
+//        ***   ***
+//        *********
+//        * ** ** *
+//        *********
+//        *********
 
-/** 시간측정 end */
-//        long afterTime = System.currentTimeMillis();
-//        long secDiffTime = (afterTime - beforeTime)/1000;
-//        System.out.println("시간차이(m) : "+secDiffTime);
-
-/** 자료구조 */
-//    static Stack<Integer> stack = new Stack<>();
-//    static Queue<Integer> queue = new LinkedList<>();
-//    static ArrayList<Integer> list = new ArrayList<>();
-//    static ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
-
-//    static HashMap<Integer, Integer> xMap = new HashMap<>();
-//    static HashMap<String, Integer> yMap = new HashMap<>();
-
-//    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//    static StringBuilder sb = new StringBuilder();
-
-//    static int[][] positionArr = null;
-//    static ArrayDeque<Integer> deque = new ArrayDeque<>();
-
-//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-//        int A = Integer.parseInt(st.nextToken());
-
-//        while(true) {
-//            String[] arr = br.readLine().split(" ");
-//
-//            if(!br.ready()) break;
-//        }
+/** 27 */
+//        ***************************
+//        * ** ** ** ** ** ** ** ** *
+//        ***************************
+//        ***   ******   ******   ***
+//        * *   * ** *   * ** *   * *
+//        ***   ******   ******   ***
+//        ***************************
+//        * ** ** ** ** ** ** ** ** *
+//        ***************************
+//        *********         *********
+//        * ** ** *         * ** ** *
+//        *********         *********
+//        ***   ***         ***   ***
+//        * *   * *         * *   * *
+//        ***   ***         ***   ***
+//        *********         *********
+//        * ** ** *         * ** ** *
+//        *********         *********
+//        ***************************
+//        * ** ** ** ** ** ** ** ** *
+//        ***************************
+//        ***   ******   ******   ***
+//        * *   * ** *   * ** *   * *
+//        ***   ******   ******   ***
+//        ***************************
+//        * ** ** ** ** ** ** ** ** *
+//        ***************************
