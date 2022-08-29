ffe7d72c7545bcd41422c7649633f6481d93dea5
snf01003 <snf01003@naver.com>
2022-08-29 17:27:57 +0900

    ..

--- studyExem/Main.java
+++ studyExem/Main.java
@@ -3,30 +3,45 @@ package studyExem;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
+    static LinkedList<String> list = new LinkedList<>();
+
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-		/*
-		  -1000000 ~ 1000000
-		  기준점 0 = index 100000 으로 생각
-		*/
-        boolean[] arr = new boolean[2000001];
+        String str = br.readLine();
+        String[] arr = str.split("");
+        for(int i=0; i<arr.length; i++) {
+            list.add(arr[i]);
+        }
 
         int N = Integer.parseInt(br.readLine());
+        String temp1, temp2 = null;
+        while(N-->0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            temp1 = st.nextToken();
+            if(temp1.equals("P")) temp2 = st.nextToken();
+            else temp2 = "";
 
-        for(int i = 0; i < N; i++) {
-            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
-        }
+            System.out.println(temp1);
+            System.out.println(temp2);
 
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i]) {
-                sb.append((i - 1000000)).append('\n');
+            switch(temp1) {
+                case "P" :
+                    list.add(temp2);
+                    break;
+                case "L" : //
+
+                    break;
+                case "D" :
+
+                    break;
+                default :
+                    break;
             }
         }
-        System.out.print(sb);
     }
 }
