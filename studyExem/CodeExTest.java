e0404aa126dfdb26d587f4728280079ac264e716
snf01003 <snf01003@naver.com>
2022-10-04 15:29:03 +0900

    ..

--- studyExem/CodeExTest.java
+++ studyExem/CodeExTest.java
@@ -1,7 +1,7 @@
 package studyExem;
 
+import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.ListIterator;
 
 public class CodeExTest {
     public static void main(String[] args) {
@@ -12,17 +12,27 @@ public class CodeExTest {
         list.add(3);
         list.add(4);
 
+        Iterator<Integer> iter = list.iterator();
+        while(iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+    }
+
+    /*public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
         ListIterator<Integer> iter = list.listIterator();
         while (iter.hasNext()) {
-            iter.next();
-            iter.remove();
             System.out.print(iter.next() + " ");
         }
-
-//        while (iter.hasPrevious()) {
-//            System.out.print(iter.previous() + " ");
-//            iter.remove();
-//            System.out.print(iter.previous() + " ");
-//        }
-    }
+        System.out.println();
+        while (iter.hasPrevious()) {
+            System.out.print(iter.previous() + " ");
+        }
+    }*/
 }
--- studyExem/Main.java
+++ studyExem/Main.java
@@ -1,44 +1,30 @@
 package studyExem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Main {
-    static LinkedList<String> list = new LinkedList<>();
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String str = br.readLine();
-        String[] arr = str.split("");
-        for(int i=0; i<arr.length; i++) {
-            list.add(arr[i]);
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551182/mcatInfoService/getPaymentNonPaymentList?ServiceKey=z2kDCblioWGus1wRE01KHMPXHKyBF%2FXeQRWgUMAUAhTKK5F61DuDlHgdfbpUMO8UKyvn1FL3iJxOY4VedrKIpA%3D%3D&numOfRows=10&pageNo=1&itmNm=''"); /*URL*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
         }
-
-        int N = Integer.parseInt(br.readLine());
-        String temp1, temp2 = null;
-        while(N-->0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            temp1 = st.nextToken();
-            if(temp1.equals("P")) temp2 = st.nextToken();
-            else temp2 = "";
-
-            switch(temp1) {
-                case "P" :
-                    list.add(temp2);
-                    break;
-                case "L" : //
-
-                    break;
-                case "D" :
-
-                    break;
-                default :
-                    break;
-            }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
         }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
     }
 }
