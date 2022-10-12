d229f06b455975271d79813b265dc0186f5206cf
snf01003 <snf01003@naver.com>
2022-10-12 17:11:53 +0900

    ..

--- studyExem/Main.java
+++ studyExem/Main.java
@@ -1,33 +1,53 @@
 package studyExem;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<int[]> list = new ArrayList<>();
-    static ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
-    public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        String inputData = scan.nextLine();
-        String[] arr = inputData.split(" ");
+    static void solution(int A, int B) throws IOException {
+        int temp = 0;
 
-        int n = Integer.parseInt(arr[0]);
-        int k = Integer.parseInt(arr[1]);
-
-        for(int i=1; i<=n+1; i++) {
-            int[] pascal = new int[i];
-            for(int j=0; j<i; j++) {
-
-                if(i-1 == j || j == 0) {
-                    pascal[j] = 1;
-                } else {
-                    pascal[j] = list.get(i-2)[j-1] + list.get(i-2)[j];
-                }
-            }
-            list.add(pascal);
+        if (A < B) {
+            temp = B;
+            B = A;
+            A = temp;
         }
 
-        System.out.println(list.get(n)[k]);
+        int GCD = 0;
+        while (true) {
+            if (A % B == 0) {
+                GCD = B;
+                break;
+            } else {
+                temp = B;
+                B = A % B;
+                A = temp;
+            }
+        }
+
+        bw.write(GCD + "\n");
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t --> 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            while(n --> 0) {
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+
+                solution(A, B);
+            }
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
