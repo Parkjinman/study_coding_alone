a9f87765431e0440a65a324ed818e7e256991197
snf01003 <snf01003@naver.com>
2022-10-12 17:44:30 +0900

    ..

--- studyExem/CodeExTest.java
+++ studyExem/CodeExTest.java
@@ -1,38 +1,42 @@
 package studyExem;
 
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class CodeExTest {
-    public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int temp = 0;
 
-        Iterator<Integer> iter = list.iterator();
-        while(iter.hasNext()) {
-            System.out.println(iter.next());
+        if(A < B) {
+            temp = B;
+            B = A;
+            A = temp;
         }
+
+        int GCF = 0;
+        int GCM = A*B;
+        while(true) {
+            if(A % B == 0) {
+                GCF = B;
+                break;
+            } else {
+                temp = B;
+                B = A % B;
+                A = temp;
+            }
+        }
+        GCM /= GCF;
+
+        bw.write(GCF+"\n");
+        bw.write(String.valueOf(GCM));
+
+        bw.flush();
+        bw.close();
     }
-
-    /*public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-
-        ListIterator<Integer> iter = list.listIterator();
-        while (iter.hasNext()) {
-            System.out.print(iter.next() + " ");
-        }
-        System.out.println();
-        while (iter.hasPrevious()) {
-            System.out.print(iter.previous() + " ");
-        }
-    }*/
 }
--- studyExem/Main.java
+++ studyExem/Main.java
@@ -4,7 +4,40 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static void solution(int A, int B) throws IOException {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] result = null;
+
+    public static void main(String[] args) throws IOException {
+        int t = Integer.parseInt(br.readLine());
+        int temp = t-1;
+        result = new int[t];
+
+        while(t --> 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int[] resultArr = new int[n];
+
+            for(int i=0; i<n; i++) {
+                resultArr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            for(int i=0; i<n; i++) {
+                if(i+1 < n) {
+                    for (int j = i + 1; j < n; j++) {
+                        solution(resultArr[i], resultArr[j], temp-t);
+                    }
+                }
+            }
+
+            bw.write(result[temp-t] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    static void solution(int A, int B, int count) throws IOException {
         int temp = 0;
 
         if (A < B) {
@@ -25,29 +58,11 @@ public class Main {
             }
         }
 
-        bw.write(GCD + "\n");
-    }
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    public static void main(String[] args) throws IOException {
-
-        int t = Integer.parseInt(br.readLine());
-
-        while(t --> 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(br.readLine());
-
-            while(n --> 0) {
-                int A = Integer.parseInt(st.nextToken());
-                int B = Integer.parseInt(st.nextToken());
-
-                solution(A, B);
-            }
-        }
-
-        bw.flush();
-        bw.close();
+        result[count] += GCD;
     }
 }
+
+//3
+//4 10 20 30 40
+//3 7 5 12
+//3 125 15 25
