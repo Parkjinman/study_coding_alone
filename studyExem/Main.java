e99642651d6c305766300c2a1a70c6e5a4759e5d
snf01003 <snf01003@naver.com>
2022-10-14 17:50:30 +0900

    ..

--- studyExem/Main.java
+++ studyExem/Main.java
@@ -1,68 +1,77 @@
 package studyExem;
 
 import java.io.*;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int[] result = null;
+    static StringBuilder sb = new StringBuilder();
+
+    static HashMap<Character, Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-        int t = Integer.parseInt(br.readLine());
-        int temp = t-1;
-        result = new int[t];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        while(t --> 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int[] resultArr = new int[n];
+        String N = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
 
-            for(int i=0; i<n; i++) {
-                resultArr[i] = Integer.parseInt(st.nextToken());
-            }
-
-            for(int i=0; i<n; i++) {
-                if(i+1 < n) {
-                    for (int j = i + 1; j < n; j++) {
-                        solution(resultArr[i], resultArr[j], temp-t);
-                    }
-                }
-            }
-
-            bw.write(result[temp-t] + "\n");
+        String[] arr = N.split("");
+        for(int i=0; i<arr.length; i++) {
+            System.out.println((int)arr[i].charAt(0));
         }
+
+//        while(true) {
+//            if(N == 0) break;
+//
+//            sb.append(result[N % B]);
+//            N /= B;
+//        }
+//
+//        bw.write(String.valueOf(sb.reverse()));
 
         bw.flush();
         bw.close();
     }
 
-    static void solution(int A, int B, int count) throws IOException {
-        int temp = 0;
-
-        if (A < B) {
-            temp = B;
-            B = A;
-            A = temp;
-        }
-
-        int GCD = 0;
-        while (true) {
-            if (A % B == 0) {
-                GCD = B;
-                break;
-            } else {
-                temp = B;
-                B = A % B;
-                A = temp;
-            }
-        }
-
-        result[count] += GCD;
+    static void mapSetting() {
+        map.put('0', 0);
+        map.put('1', 1);
+        map.put('2', 2);
+        map.put('3', 3);
+        map.put('4', 4);
+        map.put('5', 5);
+        map.put('6', 6);
+        map.put('7', 7);
+        map.put('8', 8);
+        map.put('9', 9);
+        map.put('A', 10);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 11);
+        map.put('B', 25);
+        map.put('B', 26);
+        map.put('B', 27);
+        map.put('B', 28);
+        map.put('B', 29);
+        map.put('B', 30);
+        map.put('B', 31);
+        map.put('B', 32);
+        map.put('B', 33);
+        map.put('B', 34);
+        map.put('B', 35);
+        map.put('B', 36);
     }
 }
-
-//3
-//4 10 20 30 40
-//3 7 5 12
-//3 125 15 25
