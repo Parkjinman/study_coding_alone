25ee61eb00bf6d329babe1c831d8fa8ce678123b
jin <snf01003@ex-em.com>
2024-10-05 22:15:36 +0900

    ..

--- inflearn/algorithm/DynamicProgramming/ClimbingStairs.java
+++ inflearn/algorithm/DynamicProgramming/ClimbingStairs.java
@@ -38,4 +38,41 @@ public class ClimbingStairs {
 
         return dp[n];
     }
+
+    /**
+     * Top-down leetcode solution
+     */
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            dp[n] = 1;
+            return 1;
+        }
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        dp[n] = climbStairs(n-1) + climbStairs(n-2);
+
+        return dp[n];
+    }
+
+    /**
+     * Bottom-up leetcode solution
+     */
+     public int climbStairsBottomUp(int n) {
+         if (n == 0 || n == 1) {
+             return 1;
+         }
+
+         int[] dp = new int[n + 1];
+         dp[1] = 1;
+         dp[2] = 2;
+
+         for (int i = 3; i < n+1; i++) {
+             dp[i] = dp[i-1] + dp[i-2];
+         }
+
+         return dp[n];
+     }
 }
