aa803b6a2db004db9f288306ddb1222a0967e951
jin <snf01003@ex-em.com>
2024-10-16 14:49:09 +0900

    ..

--- inflearn/algorithm/Tree/BFS.java
+++ inflearn/algorithm/Tree/BFS.java
@@ -1,4 +1,64 @@
 package inflearn.algorithm.Tree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BFS {
+    private Queue<TreeNode> queue = new LinkedList<>();
+    private ArrayList<TreeNode> visited = new ArrayList<>();
+
+    public static void main(String[] args) {
+        BFS bfs = new BFS();
+        TreeNode root;
+        root = new TreeNode("1");
+        root.left = new TreeNode("2");
+        root.right = new TreeNode("3");
+        root.left.left = new TreeNode("4");
+        root.left.right = new TreeNode("5");
+        root.right.right = new TreeNode("6");
+        bfs.bfs(root);
+    }
+
+    public void bfs(TreeNode root) {
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode currentNode = queue.poll();
+            visited.add(currentNode);
+
+            if (currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+
+        for (TreeNode node : visited) {
+            System.out.println(node.value);
+        }
+    }
+
 }
+// [1, 2, 3, 4, 5, 6]
+
+//from collections import deque
+//
+//def bfs(root):
+//        visited = []
+//        if root is None:
+//        return 0;
+//    q = deque()
+//    q.append(root)
+//    while q:
+//        cur_node = q.popleft()
+//        visited.append(cur_node.value)
+//
+//        if cur_node.left:
+//        q.append(cur_node.left)
+//        if cur_node.right:
+//        q.append(cur_node.right)
+//    return visited
---
+++ inflearn/algorithm/Tree/DFS.java
@@ -0,0 +1,76 @@
+package inflearn.algorithm.Tree;
+
+class TreeNode {
+    String value;
+    TreeNode left, right;
+
+    TreeNode(String x) {
+        value = x;
+    }
+}
+
+public class DFS {
+
+    public static void main(String[] args) {
+        DFS dfs = new DFS();
+        TreeNode root;
+        root = new TreeNode("A");
+        root.left = new TreeNode("B");
+        root.right = new TreeNode("C");
+        root.left.left = new TreeNode("D");
+        root.left.right = new TreeNode("E");
+        root.right.right = new TreeNode("F");
+        root.left.left.left = new TreeNode("G");
+        root.left.left.right = new TreeNode("H");
+
+        System.out.println("Preorder Traversal");
+        dfs.dfsPreorder(root);
+        System.out.println("Inorder Traversal");
+        dfs.dfsInorder(root);
+        System.out.println("Postorder Traversal");
+        dfs.dfsPostorder(root);
+    }
+
+    /**
+     * 전위순회 (Preorder Traversal)
+     * @param currentNode
+     */
+    public void dfsPreorder(TreeNode currentNode) {
+        if (currentNode == null) {
+            return;
+        }
+
+        System.out.println(currentNode.value);
+        dfsPreorder(currentNode.left);
+        dfsPreorder(currentNode.right);
+    }
+
+    /**
+     * 중위순회 (Inorder Traversal)
+     * @param currentNode
+     */
+    public void dfsInorder(TreeNode currentNode) {
+        if (currentNode == null) {
+            return;
+        }
+
+        dfsInorder(currentNode.left);
+        System.out.println(currentNode.value);
+        dfsInorder(currentNode.right);
+    }
+
+    /**
+     * 후위순회 (Postorder Traversal)
+     * @param currentNode
+     */
+    public void dfsPostorder(TreeNode currentNode) {
+        if (currentNode == null) {
+            return;
+        }
+
+        dfsPostorder(currentNode.left);
+        dfsPostorder(currentNode.right);
+        System.out.println(currentNode.value);
+    }
+
+}
